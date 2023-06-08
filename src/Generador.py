695835648d70e476c6ab115f1969e34ea314be0a
arielvergarautn <avergara@intercept.cloud>
2023-06-08 04:12:29 +0200

    Creando la clase abstracta modulo y los distintos tipos de modulos requeridos

---
+++ src/Generador.py
@@ -0,0 +1,9 @@
+import Modulo
+
+class Generador(Modulo):
+    def __init__(self, url_destino):
+        self.url_destino = url_destino
+
+    def ejecutar(self):
+        # TODO Implementar este metodo para generar un CSV
+        pass
---
+++ src/Validador.py
@@ -0,0 +1,6 @@
+import Modulo
+
+class Validador(Modulo):
+    def ejecutar(self):
+        # TODO Implementar este metodo
+        pass
