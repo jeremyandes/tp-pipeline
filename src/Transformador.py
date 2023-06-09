bebb15aeefaf09f74b3ff91851a7e6122bda8fcd
juano83dev <jcherviere@gmail.com>
2023-06-09 18:29:39 -0300

    clase transformador version inicial

--- src/Transformador.py
+++ src/Transformador.py
@@ -1,12 +1,24 @@
-import Modulo
+# Juan Andres Cherviere version 1
+import datetime
 
+class Transformador:
+    def __init__(self):
+        pass
 
-class Transformador(Modulo):
+    def formatear(self, data, columna, tipo_formateo):
+        if tipo_formateo == "mayusculas":
+            valor_formateado = data[columna].upper()
+        elif tipo_formateo == "minusculas":
+            valor_formateado = data[columna].lower()
+        elif tipo_formateo == "quitar_espacios":
+            valor_formateado = data[columna].strip()
+        elif tipo_formateo == "invertir":
+            valor_formateado = data[columna][::-1]
+        elif tipo_formateo == "fecha_dmy":
+            fecha = datetime.datetime.strptime(data[columna], "%Y-%m-%d")
+            valor_formateado = fecha.strftime("%d/%m/%Y")
+        else:
+            # Si el tipo de formateo no coincide con ninguna opción, no se realiza ninguna modificación
+            valor_formateado = data[columna]
 
-    @staticmethod
-    def create_instance():
-        return Transformador()
-
-    def ejecutar(self, context):
-        # TODO Implementar este metodo
-        print("Transformador executed")
+        data[columna] = valor_formateado
