97bde6d2ec26acfe68e3a8adfa35f63a12e18ab1
jeremyandes <jeremyandes25@gmail.com>
2023-06-10 19:49:22 -0300

    Solucion de conflictos

--- src/Generador.py
+++ src/Generador.py
@@ -5,11 +5,6 @@ class Generador(Modulo):
     def __init__(self, url_destino):
         self.url_destino = url_destino
 
-<<<<<<< HEAD
-    def ejecutar(self):
-         # TODO Implementar este metodo
-        pass
-=======
     @staticmethod
     def create_instance(url_destino):
         return Generador(url_destino)
@@ -17,4 +12,3 @@ class Generador(Modulo):
     def execute(self, context):
         # TODO Implementar este metodo
         print("Generador executed")
->>>>>>> bebb15aeefaf09f74b3ff91851a7e6122bda8fcd
