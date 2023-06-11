31c93e5b0fb75242e28d9d5dded66b31cb553d00
arielvergarautn <avergara@intercept.cloud>
2023-06-11 15:25:26 +0200

    Renombrando componente pipeline y creando un main.py

--- src/Modulo.py
+++ src/Modulo.py
@@ -1,8 +1,8 @@
 from abc import abstractmethod
-import ComponentPipeline
+import ComponentePipeline
 
 
-class Modulo(ComponentPipeline):
+class Modulo(ComponentePipeline):
     @abstractmethod
     def ejecutar(self, context):
         pass
