3faa44d6a25ed8de0902437b506f56ea9e30abae
juano83dev <jcherviere@gmail.com>
2023-06-16 09:33:21 -0300

    Clase agregador version 3

--- src/Agregador.py
+++ src/Agregador.py
@@ -1,34 +1,78 @@
-#Agregador version 1
-from typing import List
+# Juan Andres Cherviere version 3
 from Context import Context
 from Data import Data
+import pandas as pd
 
 class Agregador:
-    def __init__(self, lista_data):
-        self.lista_data = lista_data
+    context: Context
 
-    def cantidad_encuestas_por_paraje(self):
-        encuestas_por_paraje = {} # diccionario, para guardar el recuento
+    def __init__(self, context: Context):
+        self.context = context
 
-        for fila in self.lista_data:
-            paraje = fila.paraje
-            if paraje in encuestas_por_paraje:
-                encuestas_por_paraje[paraje] = encuestas_por_paraje[paraje] + 1
+    def contar(self) -> Context:
+        parajes = {}
+        for data in self.context.get_data():
+            paraje = data.get_paraje()
+            if paraje in parajes:
+                parajes[paraje] += 1
             else:
-                encuestas_por_paraje[paraje] = 1
+                parajes[paraje] = 1
 
-        return encuestas_por_paraje
+        resumen = []
+        for paraje, cantidad_encuestas in parajes.items():
+            resumen.append([paraje, cantidad_encuestas])
 
-    def cantidad_personas_por_paraje(self):
-        personas_por_paraje = {}
+        resumen_context = Context()
+        for row in resumen:
+            data = Data(0, None, None, row[0], row[1])
+            resumen_context.add_data(data)
 
-        for fila in self.lista_data:
-            paraje = fila.paraje
-            cantidad_personas = fila.cantidad_personas
-            if paraje in personas_por_paraje:
-                personas_por_paraje[paraje] = personas_por_paraje[paraje] + cantidad_personas
+        return resumen_context
+
+    def sumar(self) -> Context:
+        parajes = {}
+        for data in self.context.get_data():
+            paraje = data.get_paraje()
+            cantidad_personas = data.get_cantidad_personas()
+            if paraje in parajes:
+                parajes[paraje] += cantidad_personas
             else:
-                personas_por_paraje[paraje] = cantidad_personas
+                parajes[paraje] = cantidad_personas
 
-        return personas_por_paraje
+        resumen = []
+        for paraje, cantidad_personas in parajes.items():
+            resumen.append([paraje, cantidad_personas])
 
+        resumen_context = Context()
+        for row in resumen:
+            data = Data(0, None, None, row[0], row[1])
+            resumen_context.add_data(data)
+
+        return resumen_context
+
+
+'''Ejemplo:
+
+from Context import Context
+from Data import Data
+from Agregador import Agregador
+
+# Crear un contexto con datos de ejemplo
+context = Context()
+context.create_example_list()
+
+# Crear una instancia de Agregador
+agregador = Agregador(context)
+
+# Realizar el conteo de encuestas por paraje
+resumen_contar = agregador.contar()
+print("Resumen contar:")
+for data in resumen_contar.get_data():
+    data.print_data()
+
+# Realizar la suma de cantidad de personas por paraje
+resumen_sumar = agregador.sumar()
+print("Resumen sumar:")
+for data in resumen_sumar.get_data():
+    data.print_data()
+'''
