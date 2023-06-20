711b3c7fba7542b5620a2f5c12d5ee512340a5c3
arielvergarautn <avergara@intercept.cloud>
2023-06-20 21:04:33 +0200

    Agregador personas por paraje

--- src/AgregadorEncuestasPorParaje.py
+++ src/AgregadorEncuestasPorParaje.py
@@ -7,7 +7,7 @@ from DataEncuentasPorParaje import DataEncuentasPorParaje
 
 
 class AgregadorEncuestasPorParaje(ComponentePipeline):
-    def agruparEncuestasPorPeraje(self, lista_data: List(Data)) -> List(DataEncuentasPorParaje):
+    def agruparEncuestasPorParaje(self, lista_data: List(Data)) -> List(DataEncuentasPorParaje):
         resultado = []
         # TODO: Agrupar
         resultado.append(DataEncuentasPorParaje("paraje1", 4))
@@ -16,7 +16,7 @@ class AgregadorEncuestasPorParaje(ComponentePipeline):
 
     def ejecutar(self, context: ContextoGenerico):
         print("Ejecutando agregador encuestas por paraje")
-        new_lista_data = self.agruparEncuestasPorPeraje(context.get_data())
+        new_lista_data = self.agruparEncuestasPorParaje(context.get_data())
         new_context = ContextEncuestasPorParaje()
         new_context.set_data(new_lista_data)
         print("Fin ejecucion agregador encuestas por paraje")
