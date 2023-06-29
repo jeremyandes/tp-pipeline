e0f18d87f778644c33069a735d4bac3803b1eb23
jeremyandes <jeremyandes25@gmail.com>
2023-06-29 16:02:07 -0300

    Feature(Main): Agregado menu para elegir archivo CSV inicial

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,9 @@
+import os
 from AgregadorEncuestasPorParaje import AgregadorEncuestasPorParaje
 from AgregadorPersonasPorParaje import AgregadorPersonasPorParaje
 from Context import Context
 from Data import Data
+from EstadoEncuesta import EstadoEncuesta
 from Extractor import Extractor
 from FiltroEstado import FiltroEstado
 from FormateadorFecha import FormateadorFecha
@@ -11,14 +13,40 @@ from Pipeline import Pipeline
 from Selector import Selector
 from Validador import Validador
 
+# Leemos todos los archivos del directorio actual
+filenames = os.listdir("./")
+csv_files = []
+for file in filenames:
+    # Si matchea con la extensión ".csv" lo agregamos
+    if ".csv" in file:
+        csv_files.append(file)
+
+# Si no existen archivos, cortamos la ejecución.
+if(len(csv_files) == 0):
+    print("No encontramos archivos CSV para iniciar el pipeline.")
+    exit()
+
+print(f"Encontramos {len(csv_files)} archivos CSV. ¿Cuál desea elegir para iniciar el pipeline?:\n")
+
+current_csv = 0
+
+for csv in csv_files:
+    print(f"({current_csv + 1}) | {csv}")
+    current_csv = current_csv + 1
+selected_option = int(input('\n\nIngresá el número de la opción: '))
+
+# Si la opción no existe
+if(not selected_option or len(csv_files) < selected_option - 1):
+    print("Opción inexistente.")
+    exit()
+
+selected_csv = csv_files[selected_option - 1]
+
 # Creando contexto inicial
 context = Context()
 
-
-archivo = input('¿Cual es el nombre del archivo de entrada?')
-print(archivo)
 # Creando un componente extractor
-extractor = Extractor(archivo)
+extractor = Extractor(selected_csv)
 
 # Creando un componente generador campo vacio
 generadorCamposVacios = Generador("campos_vacios.csv")
@@ -27,7 +55,7 @@ generadorCamposVacios = Generador("campos_vacios.csv")
 validador = Validador(Data.get_columns_for_dataframe())
 
 # Creando un componente formateador fecha
-formateador_fecha = FormateadorFecha("fecha_inicial", '%d/%m/%Y')
+formateador_fecha = FormateadorFecha("fecha_inicial", '%d-%m-%Y')
 
 # Creando un componente formateador mayusculas paraje
 formateador_mayusculas = FormateadorMayusculas("paraje")
@@ -36,7 +64,7 @@ formateador_mayusculas = FormateadorMayusculas("paraje")
 selector = Selector()
 
 # Creando un componente filtro estado por completas
-filtro_estado_completas = FiltroEstado("Completa")
+filtro_estado_completas = FiltroEstado(EstadoEncuesta.COMPLETA)
 
 # Creando un pipeline para agregador encuestas por paraje
 agregador_encuestas_por_paraje = AgregadorEncuestasPorParaje()
